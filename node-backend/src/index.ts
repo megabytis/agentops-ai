063e7b70913ca0bb4614f0b40fe442c277d9d411
megabytis <166828632+megabytis@users.noreply.github.com>
2026-05-01 19:39:53 +0530

    feat: enable CORS in node-backend

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import route from "./api/routes";
 
 const app = express();
 
+app.use(cors());
 app.use("/api/v1", route);
 
 const PORT = 3000;
